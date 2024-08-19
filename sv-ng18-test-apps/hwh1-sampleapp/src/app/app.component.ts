62fa04b26e6acb8da9641f498cdf3037533f258b
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-08-19 17:48:28 +0530

    Using the component from Library

--- sv-ng18-test-apps/hwh1-sampleapp/src/app/app.component.ts
+++ sv-ng18-test-apps/hwh1-sampleapp/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HwH1Component } from '@viswanathaswamy-pk-techskillz-academy/hw-nglib';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [HwH1Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
